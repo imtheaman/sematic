2d17bb8b9c2bc4cabc258ecc82a42143a3703205
The Aman <urtheaman@gmail.com>
2022-04-02 19:55:46 +0530

    contact page will be completed today

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useRef } from "react";
-import Logo from "../components/Logo";
-import MainScreen from "../components/MainScreen";
-import NavBar from "../components/NavBar";
-import SideBar from "../components/SideBar";
+import Logo from "../components/home/Logo";
+import MainScreen from "../components/home/MainScreen";
+import NavBar from "../components/home/NavBar";
+import SideBar from "../components/home/SideBar";
 
 const Home: NextPage = () => {
   const maintenanceRef = useRef(100);
@@ -17,10 +17,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Sematic: Track your progress in the most efficient way.</title>
       </Head>
-      <div className="p-3 bg-yellow-500 rounded-lg animate-bounce text-[#fff] mt-2 inline absolute top-0 right-[3rem] rotate-45">
-        Live Maintenance
+      {/* Maintenace mode */}
+      <div className="p-3 bg-yellow-500 rounded-lg animate-ping text-[#fff] mt-2 inline absolute top-0 right-[3rem] rotate-45">
+        In Live Maintenance
       </div>
-      <main className="bg-white flex relative shadow-xl max rounded-2xl min-h-[100vh] overflow-hidden">
+
+      <main className="bg-white flex relative shadow-xl rounded-2xl min-h-[100vh] overflow-hidden">
         <Logo />
         <NavBar />
         <MainScreen />

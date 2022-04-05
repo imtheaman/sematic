d9e5137bc5c75831d6f84b8655b5384a1a8cee46
The Aman <urtheaman@gmail.com>
2022-04-05 16:57:44 +0530

    changed styles

--- components/insights/MainScreen.tsx
+++ components/insights/MainScreen.tsx
@@ -12,7 +12,7 @@ const MainScreen: NextPage = () => {
       <div className="w-full grid grid-cols-2 gap-4 h-60">
         <MapView />
       </div>
-      <div className="bg-red-500 w-full h-24 absolute top-70 left-0 flex items-center text-4xl text-[#fff] justify-center">
+      <div className="bg-red-500 w-full h-24 absolute top-120 left-0 flex items-center text-4xl text-[#fff] justify-center">
         ⚠ This page will take more time.<br />thinking about the styles of charts. ⚠
       </div>
     </Fragment>

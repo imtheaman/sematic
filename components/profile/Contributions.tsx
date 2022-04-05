b1e48ff8ff87a89f8787bd37be57e7b33bc0315b
The Aman <urtheaman@gmail.com>
2022-04-05 16:53:24 +0530

    Changed data

--- components/profile/Contributions.tsx
+++ components/profile/Contributions.tsx
@@ -4,12 +4,6 @@ import OneMonthContributions from "./OneMonthContributions";
 
 const Contributions: NextPage = () => {
   const data = {
-    April: [
-      [0, 0, 0, 0, 0, 6, 0],
-      [0, 0, 0, 0, 0, 0, 8],
-      [4, 2, 4, 0, 0, 0, 10],
-      [8, 6, 4, 2, 10, 0, 10],
-    ],
     May: [
       [10, 10, 8, 4, 2, 10, 2],
       [0, 4, 2, 10, 0, 2, 10],

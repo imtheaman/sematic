9a2a868faaae34bfcbea942364bb8af943759a3f
The Aman <urtheaman@gmail.com>
2022-04-28 17:45:08 +0530

    body padding added

--- components/home/ProjectCard.tsx
+++ components/home/ProjectCard.tsx
@@ -10,7 +10,7 @@ const ProjectCard: NextPage<ProjectProps> = ({ title, percent, className }) => {
       : "text-orangish bg-orange-100";
   return (
     <div
-      className={`bg-white flex w-80 flex-col justify-between shadow-md rounded-md h-36 px-6 py-4 card-animation`}
+      className={`bg-white flex max-w-80 flex-col justify-between shadow-md rounded-md h-36 px-6 py-4 card-animation`}
     >
       <div className="flex justify-between">
         <span className="text-gray-600 text-base mr-4">{title}</span>
